a224109e6c51e96cb3d6ec362ec63b056efed3a5
Jeff McRiffey <jeff.mcriffey@ambition.com>
2014-06-04 00:22:34 -0400

    start redesign

---
+++ src/template-example.js
@@ -0,0 +1,11 @@
+window.es6Example.template = {};
+
+window.es6Example.template.code = [
+    'let person = {name: \'John Smith\'};',
+    'let tpl = `My name is ${person.name}.`;',
+    '',
+    'console.log(tpl)',
+    ''
+].join('\n');
+
+window.es6Example.template.display = 'Template Literals';
