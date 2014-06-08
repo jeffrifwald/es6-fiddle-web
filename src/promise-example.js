8b924c756c4d539fbd3d9e5778fb3e736e2d4532
Jeff McRiffey <jeff.mcriffey@ambition.com>
2014-06-08 00:49:05 -0400

    promise

---
+++ src/promise-example.js
@@ -0,0 +1,20 @@
+window.es6Example.promise = {};
+
+window.es6Example.promise.code = [
+    'var longFn = function() {',
+    '\treturn new Promise(function(res, rej) {',
+    '\t\tsetTimeout(res, 1000);',
+    '\t});',
+    '};',
+    '',
+    'var coolFn = function() {',
+    '\tconsole.log(\'cool\');',
+    '};',
+    '',
+    '// logs cool after 1 second',
+    'longFn().then(coolFn);'
+].join('\n');
+
+window.es6Example.promise.display = 'Promises';
+
+
