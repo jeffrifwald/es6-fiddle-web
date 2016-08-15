f2fb90681a2bc3cc4ae4f8a949e61a3e4d4643e7
Jeff McRiffey <jmcriffey@gmail.com>
2016-08-15 16:11:01 -0400

    remove dotenv

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express'),
+    compression = require('compression'),
+    bodyParser = require('body-parser'),
     pkg = require('./package.json'),
     api = require('./api'),
     app = express(),
     port = Number(process.env.PORT || 5001);
 
-app.use(express.compress());
-app.use(express.logger());
-app.use(express.json());
-
+app.use(compression());
+app.use(bodyParser.json());
 app.use('/', express.static(__dirname + '/static'));
 
 app.get(/^\/\w+\/$/, function(req, res) {
