a8add6b068854566af8b10ee95a0bf78d284542a
Kaustubh Mallik <kaustubh.mallik@flipkart.com>
2017-01-15 08:41:58 +0530

    Checking for hostname not be 'localhost' or '127.0.0.1' before redirecting

--- src/redirect_traffic.js
+++ src/redirect_traffic.js
@@ -1,4 +1,4 @@
 // redirect traffic to HTTPS from HTTP
-if (location.hostname != 'localhost' && location.hostname != 'localhost' && location.protocol != 'https:') {
+if (location.hostname != 'localhost' && location.hostname != '127.0.0.1' && location.protocol != 'https:') {
     location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
 }
