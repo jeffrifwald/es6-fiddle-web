a4e4e4f9c337858a42cf831deb67be279d73eee7
Michael Canlas <michael.canlas428@gmail.com>
2017-02-15 23:54:44 -0500

    update readdir to use path resolver + update when done is called

--- test/image-cache.test.js
+++ test/image-cache.test.js
@@ -2,20 +2,28 @@ var request = require('supertest'),
     superagent = require('superagent'),
     expect = require('chai').expect,
     app = require('./../app').app,
-    fs = require('fs');
+    fs = require('fs'),
+    path = require('path');
 
-describe('GET /images/', () => {
-	fs.readdir(__dirname.replace(/test/, "static/images/"), (err, files) => {
-		files.forEach( file => {
-			it(`${file} responds with a max-age of 2628000`, (done) => {
+describe('GET /images/*', () => {
+
+	it('Should return max-age=2628000 for each image', (done) => {
+		fs.readdir(path.resolve(__dirname, "../static/images/"), (err, files) => {
+			files.forEach( (file, index, arr) => {
 				request(app)
 				.get(`/images/${file}`)
 				.expect(200)
 				.expect((res) => {
 					expect(res.headers['cache-control']).to.equal('public, max-age=2628000');
-				}).end(done);
-			});
+				}).end((err, res) => {
+					if ( err ) { return done(err); }
 
+					if ( index === arr.length - 1 ) {
+						done();
+					}
+				});
+			});
 		});
 	});
+
 });
