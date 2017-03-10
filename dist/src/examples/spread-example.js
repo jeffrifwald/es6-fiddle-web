696bea251495c410b6117a0a6ff89413c17415df
Ashok Siyani <ashok.siyani@gmail.com>
2017-03-10 12:27:54 +0530

    merged remote

---
+++ dist/src/examples/spread-example.js
@@ -0,0 +1,7 @@
+'use strict';
+
+window.es6Example.spread = {};
+
+window.es6Example.spread.code = ['function add(a, b) {', '\treturn a + b;', '}', '', 'let nums = [5, 4];', '', 'console.log(add(...nums));', ''].join('\n');
+
+window.es6Example.spread.display = 'Spread Operator';
