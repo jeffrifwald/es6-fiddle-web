Dianna/esfiddle

Create Symbol example

src/js/examples/symbol-example.js
@@ -1,0 +1,12 @@
+/* global window */
+window.es6Example = window.es6Example || {};
+window.es6Example.template = {};
+
+window.es6Example.template.code =
+`const tiger = Symbol('cat');
+const lion = Symbol('cat');
+
+console.log(tiger === lion);
+console.log(tiger == lion);`;
+
+window.es6Example.template.display = 'Symbols';

jeffrifwald/es6-fiddle-web

src/js/examples/symbol-example.js
@@ -1,0 +1,12 @@
+/* global window */
+window.es6Example = window.es6Example || {};
+window.es6Example.symbol = {};
+
+window.es6Example.symbol.code =
+`const tiger = Symbol('cat');
+const lion = Symbol('cat');
+
+console.log(tiger === lion);
+console.log(tiger == lion);`;
+
+window.es6Example.symbol.display = 'Symbols';