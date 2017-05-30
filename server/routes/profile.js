6db58b9e86a4fa53108869d98c91081edb8fd3d2
Student <jeff.kandel@gmail.com>
2017-05-30 14:16:31 -0400

    feat: show only public fiddles on profile route

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -12,14 +12,24 @@ const ensureAuthenticated = (req, res, next) => {
 
 module.exports = app => {
     app.get('/profile/:_id', (req, res) => {
-        Users.find({ _id: req.params._id }).then( user => {
-            Fiddles.find({ userId: req.params._id }).then(fiddles => {
-                res.render('authenticated', {
-                    user: user, fiddles: fiddles,
-                    message: req.flash()
-                });
+        const _id = req.params._id
+
+        if(req.user && _id === req.user._id){
+            res.redirect('/github/myProfile')
+        } else {
+            Users.findOne({ _id: _id }).then( user => {
+                Fiddles.find({
+                    userId: req.params._id,
+                    isPrivate: false
+                }).then( fiddles => {
+                    res.render('authenticated', {
+                        user: user, fiddles: fiddles,
+                        startedFiddles: [],
+                        message: req.flash()
+                    });
+                })
             })
-        })
-            .catch(e => res.status(400).send(e));
+                .catch(e => res.status(400).send(e));
+        }
     });
 }
