d0fedfed86962cea9bec751591fe0674299130e2
Student <jeff.kandel@gmail.com>
2017-05-30 16:11:12 -0400

    feat: display error if user profile is not found

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,14 +1,5 @@
-const passport = require('../auth');
 const Fiddles = require('../db/fiddles');
 const Users = require('../db/users');
-
-const ensureAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    console.log('ensureAuthenticated: failed');
-    res.redirect('/github/login');
-}
 
 module.exports = app => {
     app.get('/profile/:_id', (req, res) => {
@@ -17,19 +8,23 @@ module.exports = app => {
         if(req.user && _id === req.user._id){
             res.redirect('/github/myProfile')
         } else {
-            Users.findOne({ _id: _id }).then( user => {
-                Fiddles.find({
-                    userId: req.params._id,
-                    isPrivate: false
-                }).then( fiddles => {
-                    res.render('authenticated', {
-                        user: user, fiddles: fiddles,
-                        startedFiddles: [],
-                        message: req.flash()
+            Users.findOne({ _id: _id }, (err, user) => {
+                if (user){
+                    Fiddles.find({
+                        userId: req.params._id,
+                        isPrivate: false
+                    }).then( fiddles => {
+                        res.render('profile', {
+                            user: user, fiddles: fiddles,
+                            message: req.flash()
+                        });
+                    })
+                } else {
+                    res.render('profile', {
+                        _id: _id,
                     });
-                })
+                }
             })
-                .catch(e => res.status(400).send(e));
         }
     });
 }
