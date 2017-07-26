95a7fdfbdef50e2fb3dce4800b993d7b21629097
Josh Ghent <me@joshghent.com>
2017-07-26 12:07:42 +0100

    Removed dist folder. Ignored it in gitignore and updated procfile so heroku will build.

--- dist/src/examples/template-example.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-window.es6Example.template = {};
-
-window.es6Example.template.code = ['let person = {name: \'John Smith\'};', 'let tpl = `My name is ${person.name}.`;', '', 'console.log(tpl);', ''].join('\n');
-
-window.es6Example.template.display = 'Template Literals';
