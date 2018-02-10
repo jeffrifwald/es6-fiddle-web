a59a7db3110d82896400116f94d48dc5bd1a8586
Jigar Mehta <jigar.1101@gmail.com>
2018-02-10 11:47:13 +0530

    Load libraries inside the iframe scope for it to be used, add dependencies to be loaded along with libraries

--- src/js/add-libraries.js
+++ src/js/add-libraries.js
@@ -7,14 +7,17 @@ const librariesList = [{
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js',
   display: 'Backbone',
+  dependency: ['Underscore'],
 },
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js',
   display: 'Bootstrap 3',
+  dependency: ['jQuery'],
 },
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js',
   display: 'Bootstrap 4',
+  dependency: ['jQuery', 'Popper'],
 },
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/d3/4.9.1/d3.min.js',
@@ -27,6 +30,7 @@ const librariesList = [{
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.1/js/foundation.min.js',
   display: 'Foundation',
+  dependency: ['jQuery'],
 },
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.2/TweenMax.min.js',
@@ -43,6 +47,7 @@ const librariesList = [{
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js',
   display: 'jQuery UI',
+  dependency: ['jQuery'],
 },
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js',
@@ -67,6 +72,7 @@ const librariesList = [{
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/react/15.6.1/react-dom.min.js',
   display: 'React DOM',
+  dependency: ['React'],
 },
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/snap.svg/0.5.1/snap.svg-min.js',
@@ -74,7 +80,7 @@ const librariesList = [{
 },
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/three.js/86/three.min.js',
-  display: 'Three.js',
+  display: 'Three',
 },
 {
   url: 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
@@ -91,6 +97,10 @@ const librariesList = [{
 {
   url: 'https://cdn.zingchart.com/zingchart.min.js',
   display: 'ZingChart',
+},
+{
+  url: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.5/umd/popper.js',
+  display: 'Popper',
 }];
 
 const libraries = {
@@ -104,6 +114,16 @@ const libraries = {
           `<option value="${library.url}" data-display="${library.display}">${library.display}</option>`;
     }
   },
+
+  getLibraryDependencyUrls(selectedIndex) {
+    const dependency = librariesList[selectedIndex - 1].dependency;
+    if (dependency) {
+      return librariesList.filter(library => dependency.indexOf(library.display) !== -1)
+        .map(library => library.url);
+    }
+    return null;
+  },
+
 };
 
 module.exports = libraries;
