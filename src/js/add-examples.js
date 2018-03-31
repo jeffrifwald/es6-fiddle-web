60a9c8475b0ba2b72925850a2dbf64def2cd3814
Joshua Crass <joshua.crass@gmail.com>
2018-03-31 23:04:39 +0200

    refactored add-examples

--- src/js/add-examples.js
+++ src/js/add-examples.js
@@ -4,6 +4,8 @@ import $ from './helpers';
   NOTE: when adding new editions, you will also
   need to add an entry for the exampleSelector
   in esfiddle/src/js/index.js
+  TODO: Move edition info into config file
+  so we can use it in other places too.
 */
 
 const examples = {
@@ -12,32 +14,33 @@ const examples = {
       const el = $.getElement('.examples');
       const { es6Example, es7Example, es8Example } = window;
 
-      // ES6 Add a divider between editions
-      el.innerHTML += '<option disabled>----- ES6 Examples -----</option>';
-      // Map over each Example and add an option to the select/dropdown list
-      el.innerHTML += Object.keys(es6Example).map(
-        example =>
-          Object.prototype.hasOwnProperty.call(es6Example, example) &&
-          `<option value="${example}">ES6 - ${es6Example[example].display}</option>`,
-      );
+      const editionInfo = {
+        es6: {
+          name: 'ES2015',
+          example: es6Example,
+        },
+        es7: {
+          name: 'ES2016',
+          example: es7Example,
+        },
+        es8: {
+          name: 'ES2017',
+          example: es8Example,
+        },
+      };
 
-      // ES7 Add a divider between editions
-      el.innerHTML += '<option disabled>----- ES7 Examples -----</option>';
-      // Map over each Example and add an option to the select/dropdown list
-      el.innerHTML += Object.keys(es7Example).map(
-        example =>
-          Object.prototype.hasOwnProperty.call(es7Example, example) &&
-          `<option value="${example}">ES7 - ${es7Example[example].display}</option>`,
-      );
-
-      // ES8 Add a divider between editions
-      el.innerHTML += '<option disabled>----- ES8 Examples -----</option>';
-      // Map over each Example and add an option to the select/dropdown list
-      el.innerHTML += Object.keys(es8Example).map(
-        example =>
-          Object.prototype.hasOwnProperty.call(es8Example, example) &&
-          `<option value="${example}">ES8 - ${es8Example[example].display}</option>`,
-      );
+      Object.entries(editionInfo).map(([key, value]) => {
+        const shortName = key.toUpperCase();
+        // Add a divider between editions
+        el.innerHTML += `<option disabled>--- ${value.name} (${shortName}) Examples ---</option>`;
+        // Map over each Example and add an option to the select/dropdown list
+        el.innerHTML += Object.keys(value.example).map(
+          example =>
+            Object.prototype.hasOwnProperty.call(value.example, example) &&
+            `<option value="${example}"> ${value.example[example].display}</option>`,
+        );
+        return undefined;
+      });
     }
   },
 };
